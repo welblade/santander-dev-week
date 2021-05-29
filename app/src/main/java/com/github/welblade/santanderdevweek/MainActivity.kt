65e5588d4a007c8a9bea75512ff1229e368f5f9d
Welblade <welblade@gmail.com>
2021-05-29 15:00:16 -0300

    Layout concluído.

--- app/src/main/java/com/github/welblade/santanderdevweek/MainActivity.kt
+++ app/src/main/java/com/github/welblade/santanderdevweek/MainActivity.kt
@@ -2,10 +2,33 @@ package com.github.welblade.santanderdevweek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.notification_action -> {
+                Log.d("CLICK", "Ativando notificações.")
+                true
+            } else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
